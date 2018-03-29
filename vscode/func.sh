52495f667943fd091b0e9409fc13b14421aff33c
zeerorg <r.g.gupta@outlook.com>
2018-03-29 12:50:21 +0530

    Persisting data in home-dir/docker/vscode

--- vscode/func.sh
+++ vscode/func.sh
@@ -5,6 +5,7 @@ docker run \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -e XAUTHORITY=$XAUTHORITY \
+    -v $HOME/docker/vscode:/home/newuser \
     --name vscode \
     vscode 
 xhost -
--- vscode/start.sh
+++ vscode/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "password" | sudo -S chown -R newuser:newuser /home/newuser
 code
 sleep 2
 while [[ $(ps -A | grep code) ]]; do
